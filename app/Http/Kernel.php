0d84318384538c760cc1847bfd10abd523c8e670
Ken Ramandha <93147426+KenRamandha@users.noreply.github.com>
2025-08-13 01:20:24 +0700

    comment cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Http\Middleware\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\CorsMiddleware::class,
+        // \App\Http\Middleware\CorsMiddleware::class,
     ];
 
     /**
